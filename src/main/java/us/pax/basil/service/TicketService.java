6d5bd5aab78347a662ae4dfc19f3897756d5d07d
chen.liu <liu.chen@pax.us>
2023-07-24 15:59:12 -0400

    add getTicketingViewsTotal as getting total record and getTicketingViews as getting results

--- src/main/java/us/pax/basil/service/TicketService.java
+++ src/main/java/us/pax/basil/service/TicketService.java
@@ -1,6 +1,7 @@
 package us.pax.basil.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import org.springframework.web.bind.annotation.RequestParam;
 import us.pax.basil.dto.output.QueryResultArrayDTO;
 import org.springframework.web.multipart.MultipartFile;
 import us.pax.basil.dto.output.SqlResultDTO;
@@ -37,7 +38,22 @@ public interface TicketService extends IService<Integer>{
                                      Integer type,
                                      String createdDate,
                                      String serialNumber,
-                                     String customerId);
+                                     String customerOrganization);
+
+    QueryResultArrayDTO ticketQueryViews(Integer currentPage,
+                                    Integer sizePerPage,
+                                    String sortColumns,
+                                    String ticketId,
+                                    Integer department,
+                                    String responder,
+                                    Integer status,
+                                    Integer type,
+                                    String createdDate,
+                                    String lastResponse,
+                                    String serialNumber,
+                                    String customerOrganization,String customerId);
+
+
     QueryResultArrayDTO queryDepartment();
     QueryResultArrayDTO queryOrderType();
     QueryResultArrayDTO queryStatus();
