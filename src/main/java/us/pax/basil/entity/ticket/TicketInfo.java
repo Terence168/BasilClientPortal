f96ee8fc68b454cf00b4d33df9056c4308bf4b1b
xiaoxuan.liao <xiaoxuan.liao@pax.us>
2023-08-01 11:44:19 -0400

    Add files in TicketInfo.java.

--- src/main/java/us/pax/basil/entity/ticket/TicketInfo.java
+++ src/main/java/us/pax/basil/entity/ticket/TicketInfo.java
@@ -1,11 +1,16 @@
 package us.pax.basil.entity.ticket;
 
+import java.util.Date;
 import java.util.List;
 
 public class TicketInfo {
     private Integer moOID; //ticket id
-    private String orderStatus;//from (prep_)master_order ORDER_STATUS
     private Boolean isFromMaster; //if it's from master_order, mark it as true, else false
+    private String orderStatus;//from (prep_)master_order ORDER_STATUS
+    private Date createDate;//from (prep_)master_order Order_Date
+    private String organization;//submitters' org, not sure where to get it
+    private String submitterEmail;//submitters' email, not sure
+    private String submitterName;//submitters' name, not sure
     private Integer typeOfRepair; //use mo_oid to query from BASIL_ODS_PRD.MASTER_ORDER and BASIL_SEC_PRD.PREP_MASTER_ORDER
     private String address; //use mo_oid to query from BASIL_ODS_PRD.MASTER_ORDER and BASIL_SEC_PRD.PREP_MASTER_ORDER
     private List<String> trackingNumbers; //use mo_oid to query from BASIL_SEC_PRD.XREF_INBOUND_TRACKING
