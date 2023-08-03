02995eb5570fd0e95349704181f5780c6c901706
xiaoxuan.liao <xiaoxuan.liao@pax.us>
2023-08-03 09:39:19 -0400

    Add TicketEditObject

--- src/main/java/us/pax/basil/entity/ticket/SNInfo.java
+++ src/main/java/us/pax/basil/entity/ticket/SNInfo.java
@@ -13,7 +13,7 @@ import java.util.Date;
 @ApiModel(value = "SerialNumbers Information Object", description = "")
 public class SNInfo {
     //insert rows into PREP_XREF_MATERIALS
-    private String serialNumber;
+    private String serialNumber;//MSN_oid
 
     private Date orderDate;
 
@@ -27,7 +27,7 @@ public class SNInfo {
 
     private Integer xmOID; //If it's in master_order, fill in it
 
-    private Integer pxmOID; //if it's in prep_master order, fill in it
+    private Integer pxmOID; //if it's in prep_master_order, fill in it
 
     private Integer msnOID; //this is the master serial number which used to link to serial number
 
--- src/main/java/us/pax/basil/entity/ticket/TicketEditObject.java
+++ src/main/java/us/pax/basil/entity/ticket/TicketEditObject.java
@@ -3,8 +3,8 @@ package us.pax.basil.entity.ticket;
 import java.util.List;
 
 public class TicketEditObject {
-
+    private Integer mo_oid;
     private List<Integer> deleteSerials;//mc_oid
-    private List<SubmittingTicket> updateSerials;
-    private List<SubmittingTicket> addSerials;
+    private List<SNInfo> updateSerials;
+    private List<SNInfo> addSerials;
 }
