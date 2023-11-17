0a1ab51d982ea39fe0744f4865403498beab8989
Yuri Maidaniuk <yurii.maidaniuk@pax.us>
2023-11-17 16:36:34 -0500

    BCP-59 change column mappings and fixed the query

--- src/main/java/us/pax/basil/entity/salesorder/SalesOrder.java
+++ src/main/java/us/pax/basil/entity/salesorder/SalesOrder.java
@@ -11,18 +11,11 @@ public class SalesOrder {
     private String orderStatus;
     private LocalDate orderDate;
     private String customerPONumber;
-    private String moDescription;
+    private String description;
     private String specialInstructions;
-    private String salesPersonInitials;
-    private String salesPersonName;
+    private String salesPerson;
     private String contact;
-    private String customerName;
-    private String shipAddress3Loc;
-    private String shipAddress1;
-    private String shipAddress2;
-    private String shipAddress3;
-    private String shipAddress4;
-    private String shipPostalCode;
-    private String email;
+    private String sysproCustomerName;
+    private String shipAddress;
     private LocalDate reqShipDate;
 }
